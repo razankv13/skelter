3441c015511afc0a779ca41dca1799aa73f225e2
vishaltsolguruz <vishalt@solguruz.com>
2025-11-13 12:10:40 +0530

    feat: combine the separate patrol tests for end to end test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,14 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'presentation/login/login_screen_test.dart' as login;
-import 'presentation/login/login_with_email_password_screen_test.dart'
-    as login_with_email_password;
-import 'presentation/login/login_with_google_test.dart' as login_with_google;
 import 'presentation/profile/profile_screen_test.dart' as profile;
 
 void main() {
   group('Login Tests', login.main);
-  group('Login With Email Tests', login_with_email_password.main);
-  group('Login With Google Tests', login_with_google.main);
   group('Profile Tests', profile.main);
 }
