579ebe15451195cb6040a6a65cdfabe6c46b2e25
Ganesh Shinde <ganeshs@solguruz.com>
2025-11-19 18:15:28 +0530

    feat: add share functionality to product detail app bar

--- lib/presentation/product_detail/widgets/product_detail_app_bar.dart
+++ lib/presentation/product_detail/widgets/product_detail_app_bar.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:skelter/common/theme/text_style/app_text_styles.dart';
+import 'package:skelter/i18n/localization.dart';
+import 'package:skelter/presentation/product_detail/constant/product_detail_constants.dart';
 import 'package:skelter/utils/theme/extention/theme_extension.dart';
 import 'package:skelter/widgets/app_button/app_button.dart';
 import 'package:skelter/widgets/app_button/enums/app_button_size_enum.dart';
@@ -9,10 +12,12 @@ import 'package:skelter/widgets/app_button/enums/app_button_size_enum.dart';
 class ProductDetailAppBar extends StatelessWidget
     implements PreferredSizeWidget {
   final String category;
+  final String productId;
 
   const ProductDetailAppBar({
     super.key,
     required this.category,
+    required this.productId,
   });
 
   @override
@@ -30,6 +35,37 @@ class ProductDetailAppBar extends StatelessWidget
             .copyWith(color: context.currentTheme.textNeutralPrimary),
       ),
       centerTitle: true,
+      actions: [
+        IconButton(
+          onPressed: () {
+            _shareProductLink(
+              context,
+              productId,
+            );
+          },
+          icon: Icon(
+            TablerIcons.share_3,
+            color: context.currentTheme.iconNeutralDefault,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _shareProductLink(
+    BuildContext context,
+    String productId,
+  ) async {
+    final productUrl = Uri.https(kHost, '$kProductDetailPath/$productId');
+
+    final shareSubject = context.localization.share_product_subject;
+    final shareMessage = context.localization.share_product_message(
+      productUrl.toString(),
+    );
+
+    await Share.share(
+      shareMessage,
+      subject: shareSubject,
     );
   }
 
