fb7da7796051eec650026fe45fa7b7cfc9fba62e
Ganesh Shinde <ganeshs@solguruz.com>
2025-11-27 19:10:02 +0530

    refactor: update PriceWidget to use CurrencyFormatterExtension and replace empty space with WidgetSpan for spacing between price and rating

--- lib/presentation/product_detail/widgets/price.dart
+++ lib/presentation/product_detail/widgets/price.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skelter/common/theme/text_style/app_text_styles.dart';
 import 'package:skelter/i18n/localization.dart';
+import 'package:skelter/utils/extensions/currency_formatter_extensions.dart';
 import 'package:skelter/utils/theme/extention/theme_extension.dart';
 
 class Price extends StatelessWidget {
@@ -12,11 +13,14 @@ class Price extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text.rich(
       TextSpan(
-        text: '\$${price.toStringAsFixed(2)} ',
+        text: context.formatCurrency(price, decimalDigits: 2),
         style: AppTextStyles.p3SemiBold.copyWith(
           color: context.currentTheme.textBrandPrimary,
         ),
         children: <InlineSpan>[
+          const WidgetSpan(
+            child: SizedBox(width: 5),
+          ),
           TextSpan(
             text: context.localization.inclusive_of_taxes,
             style: AppTextStyles.p3Medium.copyWith(
