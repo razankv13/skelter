020eb6efb6427e0dc6a5fa73f0ed1caf7f65bc78
Ganesh Shinde <ganeshs@solguruz.com>
2025-12-02 18:04:39 +0530

    feat: add BiometricAuthentication widget to settings

---
+++ lib/presentation/settings/widgets/biometric_authentication.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:skelter/common/theme/text_style/app_text_styles.dart';
+import 'package:skelter/i18n/localization.dart';
+import 'package:skelter/utils/extensions/build_context_ext.dart';
+import 'package:skelter/utils/theme/extention/theme_extension.dart';
+
+class BiometricAuthentication extends StatelessWidget {
+  const BiometricAuthentication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(8),
+          bottomRight: Radius.circular(8),
+        ),
+      ),
+      leading: Icon(
+        TablerIcons.fingerprint,
+        color: context.currentTheme.iconNeutralDefault,
+      ),
+      title: Text(
+        context.localization.biometric_authentication,
+        style: AppTextStyles.p2Regular.copyWith(
+          color: context.currentTheme.textNeutralPrimary,
+        ),
+      ),
+      trailing: Icon(
+        TablerIcons.chevron_right,
+        color: context.currentTheme.iconNeutralDefault,
+      ),
+      onTap: () {
+        context.showSnackBar('Biometric authentication');
+      },
+    );
+  }
+}
