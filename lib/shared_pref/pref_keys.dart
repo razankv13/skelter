a0c0141394acabe2a386e8f5a3713abbecbf0183
Ganesh Shinde <ganeshs@solguruz.com>
2025-12-05 11:41:36 +0530

    feat: add IsBiometricEnabled preference key for precheck before login user has enrolled bio auth or not

--- lib/shared_pref/pref_keys.dart
+++ lib/shared_pref/pref_keys.dart
@@ -2,4 +2,5 @@ class PrefKeys {
   static const String kHasCompletedOnboarding = 'HasCompletedOnboarding';
   static const String kUserDetails = 'UserDetails';
   static const String kLastLocalAuthTimestamp = 'LastLocalAuthTimestamp';
+  static const String kIsBiometricEnabled = 'IsBiometricEnabled';
 }
